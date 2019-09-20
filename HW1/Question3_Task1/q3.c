a5c8a6bd29526fcf3d5a1a0ea56055691b5a87c5
gtrj0527 <36575481+gtrj0527@users.noreply.github.com>
2019-09-20 12:48:04 -0500

    Update q3.c

--- HW1/Question3_Task1/q3.c
+++ HW1/Question3_Task1/q3.c
@@ -8,16 +8,17 @@
  *  child to die            *
  ****************************/
 int main(){
-	int i, userEntry, got_pid, status;
+	int i, userEntry, status;
 	pid_t pid;
 	FILE *output;
 
-	output = fopen("output.txt", "w");
+	output = fopen("output_q3.txt", "w");
 
 	//get user input and print results to screen
 	printf("Enter the number of times you'd like the fork loop to run: ");
 	scanf("%d", &userEntry);	//Store the number the user enters
-	printf("You entered %d, so the loop will run %d times. \n\n", userEntry, userEntry);
+	printf("\n You entered %d, and your results will print to a text file titled output_q3.txt. \n", userEntry)
+	fprintf(output, "You entered %d, so the loop will run %d times. \n\n", userEntry, userEntry);
 
 	//for loop through the number of iterations requested by the user
 	for(i = 0; i < userEntry; i++)
@@ -38,8 +39,9 @@ int main(){
 			printf("I am a child and have no children. My process PID is %d, and my parent PID is %d \n", getpid(), getppid());
 			//print results to output.txt
 			fprintf(output, "Loop #%d \n", i+1);
-			fprintf(output, "I am a child and have no children. My process PID is %d, and my parent PID is %d \n", getpid(), getppid());
-		}
+			fprintf(output, "(Child) ";
+			fprintf(output, "pid: %d ", getpid());
+			fprintf(output, "ppid: %d \n\n", getppid());		}
 		
 		//parent sleeps until something happens and then displays its process PID and its parent's PID		
 		else {
@@ -48,17 +50,11 @@ int main(){
 			printf("I am a parent and have a child. My process PID is %d, and my parent PID is %d \n", getpid(), getppid());
 			//print results to output.txt
 			fprintf(output, "Loop #%d \n", i+1);
-			fprintf(output, "I am a parent and have a child. My process PID is %d, and my parent PID is %d \n", getpid(), getppid());
-		}
-		
+			fprintf(output, "(Parent) ";
+			fprintf(output, "pid: %d ", getpid());
+			fprintf(output, "ppid: %d \n\n", getppid());		}
+			break;
 	}
 	fclose(output);
 	return 0;	//0 because main defined as int
 }
-
-
-
-
-
-
-	
